c93dc446f503c51ad34caf85a92730302b493112
Div Shekhar <div@txtlabs.com>
2016-08-04 12:52:39 -0700

    NullMetrics as class as well to allow Class.newInstance()

--- api/src/main/scala/com/pagerduty/metrics/NullMetrics.scala
+++ api/src/main/scala/com/pagerduty/metrics/NullMetrics.scala
@@ -9,3 +9,13 @@ object NullMetrics extends Metrics {
   override def recordEvent(event: Event): Unit = {}
   override def count(name: String, count: Int, tags: (String, String)*): Unit = {}
 }
+
+/**
+  * In case you don't want to do metrics, wire up with this implementation which
+  * is just a list of no-ops.
+  */
+class NullMetrics extends Metrics {
+  override def histogram(name: String, value: Int, tags: (String, String)*): Unit = {}
+  override def recordEvent(event: Event): Unit = {}
+  override def count(name: String, count: Int, tags: (String, String)*): Unit = {}
+}
